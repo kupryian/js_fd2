6ace4bafd15edf6da19475b20a4c60bed7ec8b8d
kupryian <60928526+kupryian@users.noreply.github.com>
2022-11-25 14:52:54 +0300

    seller

---
+++ bracelets/js/drops.js
@@ -0,0 +1,72 @@
+const drops = [
+    {
+        size: 8,
+        price: 2,
+        backgroundColor: 'red'
+    },
+    {
+        size: 8,
+        price: 20,
+        backgroundColor: 'green'
+    },
+    {
+        size: 8,
+        price: 100,
+        backgroundColor: 'yellow'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#8d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#8d5525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#2d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+    {
+        size: 8,
+        price: 500,
+        backgroundColor: '#3d2525'
+    },
+];
