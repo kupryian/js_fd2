4d2ad5ed91953de5adbd8b0d79c80b9b187417a1
kupryian <60928526+kupryian@users.noreply.github.com>
2022-11-28 18:52:44 +0300

    lesson

--- bracelets/js/drops.js
+++ bracelets/js/drops.js
@@ -17,4 +17,119 @@ const drops = [
         price: 100,
         background: 'url(https://1nerudnyi.ru/wp-content/uploads/2019/01/kamen-lava2.jpg)'
     },
+    {   
+        name: 'Нефрит2',
+        size: 8,
+        price: 2,
+        background: 'url(img/rose_quarz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {
+        name: 'Лавовый камень',
+        size: 8,
+        price: 100,
+        background: 'url(https://1nerudnyi.ru/wp-content/uploads/2019/01/kamen-lava2.jpg)'
+    },
+    {   
+        name: 'Нефрит',
+        size: 8,
+        price: 2,
+        background: 'url(img/rose_quarz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {
+        name: 'Лавовый камень',
+        size: 8,
+        price: 100,
+        background: 'url(https://1nerudnyi.ru/wp-content/uploads/2019/01/kamen-lava2.jpg)'
+    },
+    {   
+        name: 'Нефрит',
+        size: 8,
+        price: 2,
+        background: 'url(img/rose_quarz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {
+        name: 'Лавовый камень',
+        size: 8,
+        price: 100,
+        background: 'url(https://1nerudnyi.ru/wp-content/uploads/2019/01/kamen-lava2.jpg)'
+    },
+    {   
+        name: 'Нефрит',
+        size: 8,
+        price: 2,
+        background: 'url(img/rose_quarz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {
+        name: 'Лавовый камень',
+        size: 8,
+        price: 100,
+        background: 'url(https://1nerudnyi.ru/wp-content/uploads/2019/01/kamen-lava2.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
+    {   
+        name: 'Розовый Кварц',
+        size: 8,
+        price: 20,
+        background: 'url(https://vashobereg.ru/wp-content/uploads/2019/09/Rozoviy-quartz.jpg)'
+    },
 ];
+console.log(drops)
